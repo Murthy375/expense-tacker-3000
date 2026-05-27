3f05d5a3eef291b4cff2863a329d6e26e05969e9
Sridhar Murthy K Chandra <sridharkc375@gmail>
2026-05-27 16:12:58 +0000

    db: add category table schema

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,9 +1,18 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const userTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   userName: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar({length: 255}).notNull(),
   salt: varchar({length: 225}),
 });
+
+export const categoryTable = pgTable("categories", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer().references(() => userTable.id),
+  categoryName: varchar({length:255}).notNull().unique(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow().$onUpdate(() => new Date()), // the .$onUpdate() updates the timestamp whenever the row is modified (uses the currect date + time)
+  // and the .defaultNow() sets the timestamp when the row is created
+})
